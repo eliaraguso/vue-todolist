0363ad6df1f3ff0e552a94f5f93ef07415cd7041
eliaraguso <elia.raguso@gmail.com>
2021-09-20 16:03:10 +0200

    aggiunto array di todos  e metodo per aggiungere un todo in VueJs con corrispettivo funzionamento in html

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,28 @@
- app = new Vue ({
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+// Bonus:
+// Aggiungere lo stato per ogni todo e modificare la visualizzazione dell'elemento in pagina, verde e barrato se il todo è stato fatto o rosso se non è ancora stato fatto.
+// Prevedere inoltre un pulsante per modificare lo stato del singolo todo
+// Google font: Lato
+ 
+ 
+const app = new Vue ({
     el: "#root",
     data: {
+        todos: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare schifo"
+        ],
+        newTodo: ""
         
     },
-    
-    methods: {
 
+    methods: {
+        addTodo: function() {
+            this.todos.push(this.newTodo)
+        }
     }
 
 });
